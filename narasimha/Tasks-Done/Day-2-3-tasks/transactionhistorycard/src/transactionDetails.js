78a2f387c4844a4f6a6836ed8d5819727a047e69
Narasimha-reddy-tech <vvsnreddy7@gmail.com>
2025-04-13 16:45:24 +0530

    Completed transactionhistorycard-13-4-25@4:45pm

---
+++ narasimha/Tasks-Done/Day-2-3-tasks/transactionhistorycard/src/transactionDetails.js
@@ -0,0 +1,53 @@
+let transactionDetails = [
+    {
+        title: 'Food Delivery',
+        amount: '+₹650',
+        date: 'Apr 13, 2024',
+        status: 'Completed',
+        category: '🍔 Food',
+        invoice: 'Transaction for food delivery'
+    },
+    {
+        title: 'Online Payment',
+        amount: '-₹2,400',
+        date: 'Apr 10, 2024',
+        status: 'Completed',
+        category: '🛒 Shopping',
+        invoice: 'Transaction for invoice #1234'
+    },
+    {
+        title: 'ATM Withdrawal',
+        amount: '-₹1,000',
+        date: 'Apr 12, 2024',
+        status: 'Pending',
+        category: '🏦 ATM',
+        invoice: 'Transaction for ATM withdrawal'
+    },
+    {
+        title: 'Refund',
+        amount: '+₹1,500',
+        date: 'Apr 14, 2024',
+        status: 'Failed',
+        category: '💳 Refund',
+        invoice: 'Transaction for refund #5678'
+    },
+    {
+        title: 'Salary Credit',
+        amount: '+₹25,000',
+        date: 'Apr 20, 2024',
+        status: 'Completed',
+        category: '💼 Salary',
+        invoice: 'Salary credited for April 2024'
+    },
+    {
+        title: 'Utility Bill Payment',
+        amount: '-₹1,200',
+        date: 'Apr 16, 2024',
+        status: 'Completed',
+        category: '💡 Bills',
+        invoice: 'Transaction for utility bill payment #1213'
+    },
+
+];
+
+export default transactionDetails;
