d4b2d562daf1509c9f305bf3405b137ff6e81b86
Sakhar1998 <Sakhar1998>
2025-04-14 17:39:05 +0530

    Productgrib completed 04/14/2025@05:38PM

---
+++ sakhar/ProductGrid/src/productsData.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Organic Carrots',
+    price: '₹60',
+    rating: 4.5,
+    image: '/src/assets/carrots.jpg'
+  },
+  {
+    id: 2,
+    name: 'Fresh Tomatoes',
+    price: '₹40',
+    rating: 4.0,
+    image: '/src/assets/tomatoes.jpg'
+  },
+  {
+    id: 3, 
+    name: 'Crisp Cucumbers',
+    price: '₹30',
+    rating: 5.0,
+    image: '/src/assets/cucumbers.jpg'
+  },
+  {
+    id: 4,
+    name: 'Sweet Bell Peppers',
+    price: '₹80',
+    rating: 4.8,
+    image: '/src/assets/peppers.jpg'
+  }
+];
